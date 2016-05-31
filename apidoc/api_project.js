a594ed2e39fc9eb830d679f90cdd35161dfd367a
RabidRabbit <aris@bedbugstudio.com>
2016-05-31 13:58:19 +0300

    inti

---
+++ apidoc/api_project.js
@@ -0,0 +1,18 @@
+define({
+  "name": "Cityfab API",
+  "version": "0.0.1",
+  "description": "Cityfab API v1",
+  "title": "Custom apiDoc browser title",
+  "url": "api",
+  "order": [
+    "authenticate"
+  ],
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-07-30T14:05:40.587Z",
+    "url": "http://apidocjs.com",
+    "version": "0.12.1"
+  }
+});
